8fda7e2d5836ab76f02d106a5538e8cffc084b39
soohyeon Kim <143922307+soohyeon01@users.noreply.github.com>
2025-09-24 18:51:20 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 19248 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,17 +1,20 @@
 import java.io.*;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
     /**
      * 자료구조 - stack
+     * + 자바에서 stack 클래스는 오래된 vector 기반이라 현재는 잘 안씀
+     *   대신 deque 을 스택/큐 처럼 사용함
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         int loop = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < loop; i++) {
@@ -19,33 +22,31 @@ public class Main {
             String str = st.nextToken();
 
             switch (str) {
-                case ("push"):
+                case "push":
                     int num = Integer.parseInt(st.nextToken());
                     stack.push(num);
                     break;
 
-                case ("pop"):
-                    if (stack.empty()) {
+                case "pop":
+                    if (stack.isEmpty()) {
                         bw.write(String.valueOf(-1));
                     } else bw.write(String.valueOf(stack.pop()));
                     bw.newLine();
                     break;
 
-                case ("size"):
+                case "size":
                     bw.write(String.valueOf(stack.size()));
                     bw.newLine();
                     break;
 
-                case ("empty"):
-                    if (stack.empty()) {
-                        bw.write(String.valueOf(1));
-                    } else bw.write(String.valueOf(0));
+                case "empty":
+                    bw.write(stack.isEmpty() ? String.valueOf(1) : String.valueOf(0));
                     bw.newLine();
                     break;
 
-                case ("top"):
-                    if (!stack.empty()) {
-                        bw.write(String.valueOf(stack.lastElement()));
+                case "top":
+                    if (!stack.isEmpty()) {
+                        bw.write(String.valueOf(stack.peek()));
                     } else bw.write(String.valueOf(-1));
                     bw.newLine();
                     break;

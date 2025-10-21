ec7584d810fdac0e0a2ce0ce835bb902e295b099
soohyeon Kim <143922307+soohyeon01@users.noreply.github.com>
2025-10-21 11:48:13 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14264 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,33 +7,24 @@ public class Main {
      * 'a' - 96 = 1
      */
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-
         final int r = 31;
         final int m = 1234567891;
 
-        int l = Integer.parseInt(br.readLine());    // 입력받을 문자열의 길이 
-        int[] intArr = new int[l];  // 각 자리수 숫자를 저장할 배열      
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
 
-        String hash = br.readLine();
-        char[] temp = hash.toCharArray();   // 입력받은 문자열을 char 배열로 우선 치환       
+        int l = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        long hash = 0;   // 최종 해시값을 저장할 변수
+        long pow = 1;    // 거듭 제곱의 최초값
 
         for (int i = 0; i < l; i++) {
-            intArr[i] = temp[i] - 96;   // char 배열에 각 문자를 int 배열에 숫자로 저장
-
-            for (int j = 0; j < i; j++) {
-                intArr[i] *= r; // 항 번호에 해당하는 r 거듭제곱
-            }
+            int value = str.charAt(i) - 96;
+            hash = (hash + value * pow) % m;
+            pow = (pow * r) % m;
         }
 
-        int sum = 0;
-        for (int i : intArr) {
-            sum += i;
-        }
-
-//        System.out.println(Arrays.toString(intArr));
-        System.out.println(sum);
+        System.out.println(hash);
 
 //        System.out.println(sb);
     }

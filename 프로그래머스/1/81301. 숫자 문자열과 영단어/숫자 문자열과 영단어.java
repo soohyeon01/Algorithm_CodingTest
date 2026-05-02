7f16973738e520df0bab464c3ed88fd01dceb307
soohyeon Kim <143922307+soohyeon01@users.noreply.github.com>
2026-05-02 20:07:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.21 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+
+class Solution {
+    public int solution(String s) {
+
+        // map 구조로 숫자와 문자를 짝지어서 대체하는 방법 고안
+        Map<String, Integer> map = Map.of(
+                "zero",0,
+                "one", 1,
+                "two", 2,
+                "three",3,
+                "four",4,
+                "five",5,
+                "six", 6,
+                "seven",7,
+                "eight",8,
+                "nine",9
+        );
+
+        // map을 순회하면서 문자열에 포함된 key 값을 value로 대체
+        for (String key : map.keySet()) {
+            s = s.replace(key, String.valueOf(map.get(key)));
+        }
+
+        // 결과를 정수로 변환하여 return
+        return Integer.parseInt(s);
+    }
+}
